b2056e69953f1c0c7bc5a781b3675a2b9415c1bb
kevinnathanaeltaufiek <kevin.taufiek@gmail.com>
2026-04-11 19:50:33 +0700

    init: backend invoice-api

---
+++ backend/invoice-api/src/test/java/com/kevinnathanaeltaufiek/invoice_api/InvoiceApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kevinnathanaeltaufiek.invoice_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InvoiceApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
